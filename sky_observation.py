30c9563239c18d84b7270eecef0c2298fce81921
Ícaro Abreu <136408366+oicaroabreu@users.noreply.github.com>
2023-07-12 17:38:31 -0300

    Galatic Coordinate Frame

--- sky_observation.py
+++ sky_observation.py
@@ -55,15 +55,20 @@ for body in celestial_bodies:
     planet_pos = observer_on_earth.at(t).observe(planet)
     app = planet_pos.apparent()
     ra, dec, _ = app.radec()
+    ra_deg = ra.dstr(warn=False).split('deg')[0].strip()
+    dec_deg = dec.dstr(warn=False).split('deg')[0].strip()
     print(f"\n{body}:")
-    print(ra.dstr(warn=False).split('deg')[0].strip())
-    print(dec.dstr(warn=False).split('deg')[0].strip())
-    ras.append(int(ra.dstr(warn=False).split('deg')[0].strip()))
-    decs.append(int(dec.dstr(warn=False).split('deg')[0].strip()))
+    print(ra_deg)
+    print(dec_deg)
+    ras.append(int(ra_deg))
+    decs.append(int(dec_deg))
 
 """
 Stars and Constelations
 """
+
+# Choose the constellations to plot
+names = ["Cru", "Ari", "Tau", "Gem", "Cnc", "Leo", "Vir", "Lib", "Sco", "Sgr", "Cap", "Aqr", "Psc"]
 
 with load.open(hipparcos.URL) as f:
     stars = hipparcos.load_dataframe(f)
@@ -74,7 +79,7 @@ url = ('https://raw.githubusercontent.com/Stellarium/stellarium/master'
 with load.open(url) as f:
     constellations = stellarium.parse_constellations(f)
 
-edges = [edge for name, edges in constellations for edge in edges]
+edges = [edge for name, edges in constellations for edge in edges if name in names]
 edges_star1 = [star1 for star1, star2 in edges]
 edges_star2 = [star2 for star1, star2 in edges]
 
@@ -83,9 +88,6 @@ center = observer.at(t).from_altaz(alt_degrees=alt.degrees, az_degrees=az.degree
 projection = build_stereographic_projection(center)
 
 star_positions = observer_on_earth.at(t).observe(Star.from_dataframe(stars))
-stars['x'], stars['y'] = projection(star_positions)
-
-print(stars)
 
 limiting_magnitude = 4.40
 
@@ -94,36 +96,46 @@ bright_stars = (stars.magnitude <= limiting_magnitude)
 magnitude = stars['magnitude'][bright_stars]
 marker_size = (0.5 + limiting_magnitude - magnitude) ** 2.0
 
-xy1 = stars[['ra_degrees', 'dec_degrees']].loc[edges_star1].values
-xy2 = stars[['ra_degrees', 'dec_degrees']].loc[edges_star2].values
+# Convert RA and Dec to radians
+def convert_ra(values):
+    values = list(values)
+    return [(value + 180) % 360 - 180 for value in values]
+
+ra_rad = np.deg2rad(convert_ra(ras))
+dec_rad = np.deg2rad(decs)
+
+stars['ra_degrees'] = convert_ra(stars['ra_degrees'])
+
+stars_ra_rad = np.deg2rad(stars['ra_degrees'][bright_stars])
+stars_dec_rad = np.deg2rad(stars['dec_degrees'][bright_stars])
+
+xy1 = np.deg2rad(stars[['ra_degrees', 'dec_degrees']].loc[edges_star1]).values
+xy2 = np.deg2rad(stars[['ra_degrees', 'dec_degrees']].loc[edges_star2]).values
 lines_xy = np.rollaxis(np.array([xy1, xy2]), 1)
 
+fig = plt.figure(figsize=(10, 5))
+ax = fig.add_subplot(111, projection='aitoff')
 
-ras = np.array(ras)
-decs = np.array(decs)
+ax.scatter(stars_ra_rad, stars_dec_rad,
+           s=marker_size, color='k')
 
-fig, ax = plt.subplots(figsize=(8, 8))
-ax.set_xlim(0, 360)
-ax.set_ylim(-90, 90)
-ax.set_xlabel('Longitude')
-ax.set_ylabel('Altitude')
-ax.set_title('Observation Chart')
-
-unique_longitudes = np.unique(ras)
+unique_longitudes = np.unique(ra_rad)
 colors = plt.cm.rainbow(np.linspace(0, 1, len(unique_longitudes)))
 
 for lon, color in zip(unique_longitudes, colors):
-    mask = ras == lon
-    ax.scatter(ras[mask], decs[mask], label=f'Longitude {lon}', color=color)
+    mask = ra_rad == lon
+    ax.scatter(ra_rad[mask], dec_rad[mask], label=f'Longitude {lon}', color=color)
+    
+for i, name in enumerate(celestial_bodies):
+    ax.annotate(name.split()[0], (ra_rad[i], dec_rad[i]), textcoords='offset points', xytext=(5, 5), ha='center')
+
 
 ax.add_collection(LineCollection(lines_xy, colors='#00f2'))
-ax.scatter(stars['ra_degrees'][bright_stars], stars['dec_degrees'][bright_stars],
-           s=marker_size, color='k')
 
 ax.grid(True)
+ax.set_title('Galactic Lat-Long Plot')
+ax.set_xlabel('Galactic Longitude (radians)')
+ax.set_ylabel('Galactic Latitude (radians)')
 
-for i, name in enumerate(celestial_bodies):
-    ax.annotate(name.split()[0], (ras[i], decs[i]), textcoords='offset points', xytext=(5, 5), ha='center')
-
-#fig.savefig('astro.png', bbox_inches='tight')
+fig.savefig('sky_observation.png', bbox_inches='tight')
 plt.show()
